8dfa97e333220a8cacfd34165634467199b73bc2
Richard Webb <richard.webb@helpsystems.com>
2024-09-06 17:38:22 +0100

    Is System.Reactive.Compatibility needed in build.fsx?

--- build.fsx
+++ build.fsx
@@ -37,7 +37,6 @@ nuget Fake.Windows.Chocolatey prerelease
 nuget Fake.JavaScript.Npm prerelease
 nuget Fake.Tools.Git prerelease
 nuget Mono.Cecil prerelease
-nuget System.Reactive.Compatibility
 nuget Suave
 nuget Newtonsoft.Json
 nuget System.Net.Http
